018c7a11dadd06594dedb349be8b453006817465
Akshay-Simha <102586859+Akshay-Simha@users.noreply.github.com>
2025-06-17 23:10:48 +0530

    Create feed.py

--- feed.py
+++ feed.py
@@ -10,7 +10,7 @@ with open('feed.yaml', 'r') as file:
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
-link_prefix = yaml_data['link']
+link_prefix = yaml_data['https://akshay-simha.github.io/github-actions-demo/']
 
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
 xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
